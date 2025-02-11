87a91b1258bafa5936d34e066e2cb102bd2a7ee5
cursecan <anderi.setiawan@gmail.com>
2025-02-12 01:42:34 +0700

    nesda

--- api/proxy.py
+++ api/proxy.py
@@ -19,12 +19,5 @@ def proxy():
     # Create a response object
     return Response(response.content, status=response.status_code, content_type=response.headers['Content-Type'])
 
-# Vercel requires this handler for serverless function
-def handler(event, context):
-    with app.test_request_context(event['path'], method=event['httpMethod'], data=event['body']):
-        response = app.full_dispatch_request()
-    return {
-        'statusCode': response.status_code,
-        'body': response.get_data(as_text=True),
-        'headers': dict(response.headers)
-    }
+if __name__ == '__main__':
+    app.run(debug=True)
